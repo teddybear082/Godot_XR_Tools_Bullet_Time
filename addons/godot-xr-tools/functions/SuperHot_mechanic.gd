52c7151710ba9ccc3b0fd8dfa5e4ee5eb7301913
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-09-18 12:52:41 -0400

    Update SuperHot_mechanic.gd
    -Make calculation of wait time simpler
    -add more export variables

--- addons/godot-xr-tools/functions/SuperHot_mechanic.gd
+++ addons/godot-xr-tools/functions/SuperHot_mechanic.gd
@@ -9,15 +9,21 @@ export var active := true
 export var slo_mo_ease := 20
 
 ## Slomo time scale (percentage of engine speed)
-export var slowmo_time_scale := .05
+export var slowmo_time_scale := .03
+
 ## Normal time sclae
 export var normal_time_scale = 1.0
+
+## set time to wait, in msec, before calculating slomo again
+export var time_to_check_in_msec :float = 100.0
+
+## Arbitrary Threshold to determine if enough movement to move time (in godot units)
+var slowmo_threshold : float = .01
 
 ## Hand and Camera nodepaths
 export (NodePath) var arvrcamera_path  = ""
 export (NodePath) var left_hand_path = ""
 export (NodePath) var right_hand_path = ""
-
 
 
 ## Hand and camera nodes
@@ -29,9 +35,6 @@ onready var arvrcamera_node = get_node(arvrcamera_path)
 ## Determine if slowmo itself is currently active
 var slomo = false
 
-## Arbitrary Threshold to determine if enough movement to move time (in units of movement)
-var slowmo_threshold : float = .005
-
 ## Variables used for tracking current and last positions of head and hands
 var l_hand_position : Vector3 = Vector3.ZERO
 var r_hand_position : Vector3 = Vector3.ZERO
@@ -40,8 +43,7 @@ var last_l_hand_position : Vector3 = Vector3.ZERO
 var last_r_hand_position : Vector3 = Vector3.ZERO
 var last_arvrcamera_position : Vector3 = Vector3.ZERO
 var time_elapsed_in_msecs : float = 0.0
-var current_time_in_msecs : float = 0.0
-var last_time_in_msecs : float = 0.0
+
 
 ## Tell other nodes when slomo is active or not
 signal slomo_active
@@ -54,13 +56,14 @@ func _ready():
 	last_l_hand_position = l_hand_node.global_transform.origin
 	last_r_hand_position = r_hand_node.global_transform.origin
 	last_arvrcamera_position = arvrcamera_node.global_transform.origin
-	current_time_in_msecs = OS.get_ticks_msec()
-	last_time_in_msecs = current_time_in_msecs
+	
 
 # Main handling of slomo
 func _physics_process(delta):
-	if time_elapsed() >= 10.0:
-		last_time_in_msecs = OS.get_ticks_msec()
+	
+	#if time since last check is greater than 100 miliseconds in real time / .1 second, then recalculate whether should be in slomo
+	if OS.get_ticks_msec() - time_elapsed_in_msecs >= time_to_check_in_msec:
+		time_elapsed_in_msecs = OS.get_ticks_msec()	
 		slomo = calc_slomo()
 	
 	# If player's movement is not past movement threshold, slomo stays in effect, otherwise moves to normal time scale
@@ -72,11 +75,7 @@ func _physics_process(delta):
 		#emit_signal("slomo_off")
 		Engine.time_scale = lerp(Engine.time_scale, normal_time_scale, delta * slo_mo_ease)		
 	
-func time_elapsed() -> float:
-	current_time_in_msecs = OS.get_ticks_msec()
-	time_elapsed_in_msecs = current_time_in_msecs - last_time_in_msecs
-	return time_elapsed_in_msecs
-	
+#calculate whether player's movement is enough to keep time moving or if player is steady  enough to stop time
 func calc_slomo() -> bool:
 	l_hand_position = l_hand_node.global_transform.origin
 	r_hand_position = r_hand_node.global_transform.origin
